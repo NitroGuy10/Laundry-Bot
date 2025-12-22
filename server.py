bcd1cecffcd052769808d38de78439d12a4167a7
NitroGuy10 <naviyer10@gmail.com>
2025-12-22 17:29:55 -0600

    whoops i didn't know it was gitignored

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from waitress import serve
 
-app = Flask(__name__, static_folder='site', static_url_path='')
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 
 @app.get('/')
