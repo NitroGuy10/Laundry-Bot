98790734065d9e28b3cfd1c0233be2626a51e522
NitroGuy10 <naviyer10@gmail.com>
2026-01-11 15:13:32 -0600

    Fix not running on startup

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 SCRIPT_DIR=$(cd $(dirname "$0") && pwd)
 cd $SCRIPT_DIR
 
+# Wait for connection to be good
+until ping -c1 discord.com; do sleep 5; done
+
 source venv/bin/activate
 python main.py 
